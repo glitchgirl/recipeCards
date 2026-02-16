0faafe7ed1db950f82bc265cde718eb046425ca2
Morgan Smith <londonspleen@gmail.com>
2026-02-16 10:43:52 -0500

    feat: implement first card

--- app/src/main/java/com/example/recipecards/MainActivity.kt
+++ app/src/main/java/com/example/recipecards/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.recipecards.ui.theme.RecipeCardsTheme
+import androidx.compose.material3.*
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +19,33 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RecipeCardsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    MyCard()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MyCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "Material Card Title",
+                style = MaterialTheme.typography.titleLarge
+            )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RecipeCardsTheme {
-        Greeting("Android")
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "This is a Material 3 card using Jetpack Compose."
+            )
+        }
     }
 }
